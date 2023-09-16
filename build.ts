747a36ce3861e1af9c18ea2447b19c8d2dad16af
Aldo D'Aquino <18645793+daquinoaldo@users.noreply.github.com>
2023-09-16 14:31:12 +0000

    fix deno

--- build.ts
+++ build.ts
@@ -9,46 +9,15 @@ interface Item {
   categories: string[]
 }
 
-function itemToHTML(item: Item) {
-  // cut the description to max 100 chars
-  item.description = item.description
-    .replace(/<[^>]*>?/gm, "") // remove html tags
-    .slice(0, 100) // cut to 100 characters
-    .split(" ") // split the string in words
-    .slice(0, -1) // remove the last (cropped) word
-    .join(" ") // join back into a string
-
-  // format the date
-  item.pubDate = new Date(item.pubDate)
-    .toLocaleDateString("en-US", {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    })
-
-  // create the card
-  return `
-<a href="${item.link}"><img src="${item.thumbnail}"></a>
-<h3><a href="${item.link}">${item.title}</a></h3>
-<p>${item.description}...</p>
-<sub><sup>${item.pubDate}</sup></sub>
-`
-}
-
 function getPosts(): Promise<string> {
-  return fetch(
-    `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@daquinoaldo`,
-  )
+  return fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@daquinoaldo")
     .then((res) => res.json())
     .then((mediumFeed: { items: Item[] }) =>
       mediumFeed.items
         .filter((item) => item.categories.length > 0) // remove comments (they have no categories)
-        .slice(0, 3) // keep the last three posts
-        .map((item) => itemToHTML(item)) // obtain the html for each post
-        .reduce((acc, post) =>
-          // insert them into a 1x3 table
-          acc += `<td valign="top">${post}</td>`, "<table><tr>") +
-      "</tr></table>"
+        .slice(0, 10)
+        .map((item) => `- [${item.title}](${item.link})\n`)
+        .reduce((list, entry) => list += entry)
     )
 }
 
@@ -57,18 +26,9 @@ const writeFile = (filename: string, content: string) =>
 const readFile = (filename: string) =>
   Deno.readFile(filename).then((data) => new TextDecoder("utf-8").decode(data))
 
-// read the readme
 let readme = await readFile("README.md")
-
-// update the about section
-const about = await readFile("about.md")
-readme = readme.replace(
-  /(?<=<!-- ABOUT_START -->)(.*?)(?=<!-- ABOUT_END -->)/s,
-  `\n${about}\n`,
-)
-
-// update the Medium posts
 const posts = await getPosts()
+
 readme = readme.replace(
   /(?<=<!-- MEDIUM_POSTS_START -->)(.*?)(?=<!-- MEDIUM_POSTS_END -->)/s,
   `\n${posts}\n`,
